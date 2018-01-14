d726d0e98417cdcc5522ed59a3f782f0f512f231
TheCurryMan <avinashkalpitjain@gmail.com>
2018-01-14 00:53:40 -0800

    Added Score to Speech

--- SpeechAI.py
+++ SpeechAI.py
@@ -8,6 +8,7 @@ from firebase import firebase
 import urllib.request
 from findSilences import findSilences
 import json
+from random import *
 
 DEBUG = True
 app = Flask(__name__)
@@ -26,6 +27,7 @@ def getAudio():
 
     print("https://speechai2.firebaseio.com"+'/users/'+userID+'/speeches/'+recordingID)
 
+    lastData = firebase.get('/users/'+userID+'/latestSpeech/', None)
     recordingData = firebase.get('/users/'+userID+'/speeches/'+recordingID, None)
     print(recordingData)
 
@@ -41,11 +43,28 @@ def getAudio():
 
     pausing = findSilences(recordedText)
     similarity = similar(actualText, getText(file_name))
-    wpm = getWordsPerMinute(recordedText)
+    wpm = int(getWordsPerMinute(recordedText))
     loudness = getSoundLoudness(file_name)
     print(wpm)
 
-    data = {'wpm':wpm, 'similarity':similarity, 'pausing':pausing, 'loudness':loudness}
+    score = 0.0
+    if wpm >= 120 and wpm <= 170:
+        score += .25
+    else:
+        score += randint(10,15)/100
+    score += similarity * .25
+    if pausing == 'good':
+        score += .25
+    else:
+        score += randint(10,15)/100
+    if loudness == 'quiet':
+        score += randint(10,15)/100
+    else:
+        score += .25
+
+    data = {'wpm': wpm, 'similarity': similarity, 'pausing': pausing, 'loudness': loudness, 'score':score}
+
+
     return json.dumps(data)
 
 
